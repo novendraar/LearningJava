38af25790d9a546a5bb2414eea19d89609431b13
novendraar <novendraar@gmail.com>
2025-01-07 08:34:28 +0700

    Day 01

---
+++ Day01.java
@@ -0,0 +1,33 @@
+public class Day01 {
+
+    public static void main(String[] args) {
+        System.out.println("testing");
+
+        byte tinyNumber = 1;
+        short smallNumber = 2;
+        int mediumNumber = 3;
+        long largeNumber = 4;
+
+        float mediumDecimal = 1.5f;
+        double mediumDouble = 2.6;
+
+        System.out.println(tinyNumber);
+        System.out.println(smallNumber);
+        System.out.println(mediumNumber);
+        System.out.println(largeNumber);
+        System.out.println(mediumDecimal);
+        System.out.println(mediumDouble);
+
+        boolean truthy = true;
+        boolean falsy = false;
+
+        System.out.println(truthy);
+        System.out.println(falsy);
+
+        Integer mediumObjectNumber = 5;
+        System.out.println(mediumObjectNumber);
+        
+        String word = "novend";
+        System.out.println(word.charAt(4));
+    }
+}
